7d7e3faba6f527b959936538f56e685992b2d64a
biiluu <00042124@uca.edu.sv>
2025-07-04 12:13:46 -0600

    fix; Agregar ClienteCuadrado

--- ParcialFinal/src/main/java/com/mycompany/parcialfinal/ClienteCuadrado.java
+++ ParcialFinal/src/main/java/com/mycompany/parcialfinal/ClienteCuadrado.java
@@ -4,10 +4,45 @@
  */
 package com.mycompany.parcialfinal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  * @author natyl
  */
 public class ClienteCuadrado {
-    
+    public static void main(String[] args) {
+        String host = "localhost";
+        int puerto = 5000;
+
+        try (
+            Socket socket = new Socket(host, puerto);
+            PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner sc = new Scanner(System.in);
+        ) {
+            System.out.print("Ingrese su nombre: ");
+            String nombre = sc.nextLine();
+            System.out.print("Ingrese un numero entero: ");
+            int numero = sc.nextInt();
+
+            salida.println(nombre);
+            salida.println(numero);
+
+            System.out.println("\nRespuesta del servidor:");
+            System.out.println(entrada.readLine());
+            System.out.println(entrada.readLine());
+            System.out.println(entrada.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
